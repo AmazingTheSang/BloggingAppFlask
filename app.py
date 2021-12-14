3bae74fcf9be41b1cc9e891e85e535639173b25d
AmazingTheSang <Taulasang@gmail.com>
2021-12-14 14:54:06 +0700

    da ket noi dc voi csdl

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-import re
+import sqlite3
 from flask import Flask, render_template, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-
+import re
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
@@ -18,18 +18,51 @@ class CodeSpeedyBlog(db.Model):
     num_like = db.Column(db.Integer, default=0)
     def __repr__(self):
         return self.title
-
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 db.create_all()
 db.session.commit()
-
-@app.route('/home')
 @app.route('/')
-def Welcome():
+@app.route('/login',methods=['GET', 'POST'])
+def login():
     return render_template('login.html')
-@app.route('/signup')
+@app.route('/signup',  methods=['GET', 'POST'])
 def signup():
-    return render_template('signup.html')
+     msg = ''
+     if request.method == 'POST':
+        user_name = request.form['Username']
+        user_email = request.form['email']
+        user_pwd = request.form['psw']
+        new_user = User(username=user_name,email=user_email,password=user_pwd)
+        conn = sqlite3.connect('database.db')
+        cursorObj = conn.cursor()
+        cursorObj.execute('SELECT * FROM user WHERE username = ?', (user_name, ))
+        rows = cursorObj.fetchone()
+        if rows:
+            msg = 'Username already exists !'
+            return render_template('signup.html',msg=msg)
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+',user_email):
+            msg = 'Invalid email address !'
+            return render_template('signup.html',msg=msg)
+        elif not re.match(r'[A-Za-z0-9]+', user_name):
+            msg = 'Username must contain only characters and numbers !'
+            return render_template('signup.html',msg=msg)
+        elif not user_name or not user_pwd or not user_email:
+            msg = 'Please fill out the form !'
+            return render_template('signup.html',msg=msg)
+        else:
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect('/login')
+     else:
+        msg='Please fill out the form !'
+        return render_template('signup.html',msg=msg)
 @app.route('/posts',  methods=['GET', 'POST'])
 def posts():
     if request.method == 'POST':
--- createdatabase.py
+++ createdatabase.py
@@ -1,5 +1,5 @@
 import sqlite3
 conn = sqlite3.connect('database.db')
 cursorObj = conn.cursor()
-cursorObj.execute("CREATE TABLE CodeSpeedyBlog(id INTEGER PRIMARY KEY, title VARCHAR(100) NOT NULL , content TEXT NOT NULL, posted_by  VARCHAR(20) NOT NULL,posted_on DATETIME NOT NULL, num_likes INTEGER DEFAULT 0 ) ")
+cursorObj.execute("DROP TABLE User")
 conn.commit()
